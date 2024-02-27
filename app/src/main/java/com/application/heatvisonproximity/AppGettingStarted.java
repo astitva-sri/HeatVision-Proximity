145d4ef2969f7babf881e658482ed367f29752be
Astitva Srivastava <113296789+astitva-sri@users.noreply.github.com>
2024-02-27 23:03:57 +0530

    First Commit

---
+++ app/src/main/java/com/application/heatvisonproximity/AppGettingStarted.java
@@ -0,0 +1,14 @@
+package com.application.heatvisonproximity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppGettingStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_getting_started);
+    }
+}
