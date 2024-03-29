aa24692070a9d0384e730d301c0624a297c018a9
Astitva Srivastava <113296789+astitva-sri@users.noreply.github.com>
2024-03-30 01:29:25 +0530

    Initial UI created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,17 @@ android {
 
 dependencies {
 
+    implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.annotation:annotation:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.activity:activity:1.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/application/heatvisonproximity/MainActivity.java
+++ app/src/main/java/com/application/heatvisonproximity/MainActivity.java
@@ -1,14 +1,51 @@
 package com.application.heatvisonproximity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.application.heatvisonproximity.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavBar.setBackground(null);
+
+        binding.bottomNavBar.setOnItemSelectedListener(menuItem ->{
+
+            int item = menuItem.getItemId();
+
+            if (item == R.id.navHome){
+                replaceFragment(new HomeFragment());
+            } else if (item == R.id.navGallery) {
+                replaceFragment(new GalleryFragment());
+            } else if (item == R.id.navProfile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+
+        ImageView fab = (ImageView) findViewById(R.id.notificationBell);
+        fab.setTooltipText("Notifications On/Off");
+    }
+
+    private  void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
